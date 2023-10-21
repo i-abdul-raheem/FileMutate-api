55573b4fdcce86298cfc82446eda8caef66a6406
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-10-21 10:24:13 +0500

    Get test

--- src/routes/compress-image.js
+++ src/routes/compress-image.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { CompressImage } = require("../handlers");
 
 const handler = new CompressImage();
+router.get("/", (req, res) => res.send("Test pass"));
 router.post("/", handler.compressImage);
 
 module.exports = router;
