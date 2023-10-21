1102163be2f07ccc1f515dfb9299d4eea0e32364
Abdul Raheem <abdul.raheem.30140@gmail.com>
2023-10-21 11:39:43 +0500

    increased limit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const routes = require("./routes");
 const path = require("node:path");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(fileUpload());
 app.use(cors());
 
